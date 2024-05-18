a45ad2edc51f2aa7be0366898a9df43b58434bab
nazyri <mohachevezsaborio@gmail.com>
2024-05-18 12:02:13 -0600

    diferentesManeras

---
+++ ej.js
@@ -0,0 +1,66 @@
+
+let valor= document.getElementsByClassName('valor')
+console.log(valor)
+
+
+valor[0].addEventListener('click',function () {
+        valor[0].innerHTML="⭕"
+
+    })
+
+let valor1= document.getElementsByClassName('valor1')
+console.log(valor1)
+    
+    
+    valor1[0].addEventListener('click',function () {
+        valor1[0].innerHTML="⭕"
+})
+
+let valor2= document.getElementsByClassName('valor2')
+console.log(valor2)
+        
+     valor2[0].addEventListener('click',function () {
+        valor2[0].innerHTML="✖️"
+})
+
+let valor3= document.getElementsByClassName('valor3')
+console.log(valor3)
+        
+     valor3[0].addEventListener('click',function () {
+        valor3[0].innerHTML="✖️"
+})
+
+let valor4= document.getElementsByClassName('valor4')
+console.log(valor4)
+        
+     valor4[0].addEventListener('click',function () {
+        valor4[0].innerHTML="✖️"
+})
+
+let valor5= document.getElementsByClassName('valor5')
+console.log(valor5)
+        
+     valor5[0].addEventListener('click',function () {
+        valor5[0].innerHTML="⭕"
+})
+
+let valor6= document.getElementsByClassName('valor6')
+console.log(valor6)
+        
+     valor6[0].addEventListener('click',function () {
+        valor6[0].innerHTML="⭕"
+})
+
+let valor7= document.getElementsByClassName('valor7')
+console.log(valor7)
+        
+     valor7[0].addEventListener('click',function () {
+        valor7[0].innerHTML="✖️"
+})
+
+let valor8= document.getElementsByClassName('valor8')
+console.log(valor8)
+        
+     valor8[0].addEventListener('click',function () {
+        valor8[0].innerHTML="✖️"
+})
---
+++ gato.js
@@ -0,0 +1,38 @@
+let isPlayerOne = true
+let valor = document.getElementsByClassName('valor')
+
+for (let index = 0; index < valor.length; index++) {{
+    valor[index].addEventListener('click',userMover);
+}}
+
+function userMover(e) {
+    let valorValue = e.target.innerHTML;
+    if (!valorValue.length){
+        e.target.innerHTML = isPlayerOne? '✖️':'⭕';
+        isPlayerOne = !isPlayerOne;
+
+        checkLine(0,1,2);
+        checkLine(3,4,5);
+        checkLine(6,7,8);
+        checkLine(0,3,6);
+        checkLine(1,4,7);
+        checkLine(2,5,8);
+        checkLine(0,4,8);
+        checkLine(6,4,2);
+        
+    }
+}
+
+function checkLine(c1,c2,c3) {
+    if (
+        valor[c1].innerHTML.length &&
+        valor[c1].innerHTML == valor[c2].innerHTML &&
+        valor[c2].innerHTML == valor[c3].innerHTML
+    ) {
+        showWinner(valor[c1].innerHTML);
+    }
+}
+
+function showWinner(player) {
+    document.querySelector('#resultado').innerHTML = player + 'win';
+}
